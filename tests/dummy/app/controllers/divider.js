079b5e787885e4b8ee7377b15d7250ee3201e3cb
Mike Wilson <mike1234@gmail.com>
2015-04-07 12:20:10 -0700

    Add divider demo page

---
+++ tests/dummy/app/controllers/divider.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import BaseDemoController from '../controllers/base-demo-controller';
+
+export default BaseDemoController.extend({
+  demoName: 'divider',
+  messages: Ember.A([{
+    face : 'http://lorempixel.com/50/50/people',
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    face : 'http://lorempixel.com/50/50/people',
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    face : 'http://lorempixel.com/50/50/people',
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    face : 'http://lorempixel.com/50/50/people',
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }, {
+    face : 'http://lorempixel.com/50/50/people',
+    what: 'Brunch this weekend?',
+    who: 'Min Li Chan',
+    when: '3:08PM',
+    notes: " I'll be in your neighborhood doing errands"
+  }])
+});
