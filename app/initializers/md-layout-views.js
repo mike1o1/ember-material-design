2f78cde149a5196b158d06419cfb342fb42657dd
jhr007 <john.holland.robinson@gmail.com>
2015-09-01 14:06:59 -0500

    Update for Ember 2.0 Deprecation

--- app/initializers/md-layout-views.js
+++ app/initializers/md-layout-views.js
@@ -5,9 +5,14 @@ import HasLayout from 'ember-material-design/mixins/has-layout';
 export function initialize(/* container, application */) {
   // We want to inject the flex and layout parameters to every element
   // so we don't need to customize each view or component to add it
-  Ember.View.reopen(HasFlex, HasLayout, {
-    flex: null
-  });
+  
+  // Only reopen Ember.view if it exists. 
+  // Ember.View was deprecated in Ember 2.0, but it could be reincluded with an addon
+  if(Ember.View) {
+    Ember.View.reopen(HasFlex, HasLayout, {
+      flex: null
+    });
+  }
 
   Ember.Component.reopen(HasFlex, HasLayout, {
     flex: null
